73790857169096ec898a389791be77aeca7b8f0f
smichaelonline <stephaniemichael001@gmail.com>
2022-09-22 15:08:16 -0400

    add folders

---
+++ data/skill-data.js
@@ -0,0 +1,12 @@
+const skills = [
+  {text: 'CSS', learned: true},
+  {text: 'Javascript', learned: true},
+  {text: 'HTML', learned: true},
+  {text: 'Debugging', learned: true},
+  {text: 'DOM Manipulation', learned: true},
+  {text: 'Python', learned: false},
+]
+
+export { 
+	skills
+}
