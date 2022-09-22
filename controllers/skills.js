3f21934169c8a3ead76a9c5d415edfaba025a276
smichaelonline <stephaniemichael001@gmail.com>
2022-09-22 18:30:38 -0400

    add method override

--- controllers/skills.js
+++ controllers/skills.js
@@ -5,7 +5,8 @@ function index(req, res) {
   Skill.find({})
   .then (skills => {
   res.render('skills/index', {
-    skills: skills
+    skills: skills,
+    time: req.time
     })
   })
   .catch(error => {
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
+import methodOverride from "method-override"
 import './config/database.js'
 
 // import routers
@@ -22,6 +23,10 @@ app.set(
 app.set('view engine', 'ejs')
 
 // middleware
+app.use(function(req, res, next) {
+  req.time = new Date(). toLocaleTimeString()
+  next ()
+})
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -30,7 +35,8 @@ app.use(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
   )
 )
-app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false }))
 
 // mounted routers
 app.use('/', indexRouter)
