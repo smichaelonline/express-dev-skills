4e7ce92fdc98576a2d28bc8a0c3f5bc8029128a5
smichaelonline <stephaniemichael001@gmail.com>
2022-09-22 16:51:12 -0400

    create render function for new skill

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,12 +1,24 @@
-import { skills } from "../data/skill-data.js";
+import { Skill } from '../models/skill.js'
 
 
 function index(req, res) {
+  Skill.find({})
+  .then (skills => {
   res.render('skills/index', {
     skills: skills
+    })
   })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function newSkill(req,res) {
+  res.render('skills/new')
 }
 
 export {
   index,
+  newSkill as new, 
 }
--- routes/skills.js
+++ routes/skills.js
@@ -4,6 +4,7 @@ import * as skillsCtrl from '../controllers/skills.js'
 const router = Router()
 
 router.get('/', skillsCtrl.index)
+router.get('/new', skillsCtrl.new)
 
 export {
   router,
